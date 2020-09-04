c10093be1ba62ab726002e612594f8886492884b
Akshay Nachappa <akshaynachappa@gmail.com>
2020-09-04 23:00:14 +0530

    Update Coord_finder.py

--- Coord_finder.py
+++ Coord_finder.py
@@ -2,4 +2,5 @@ import pyautogui as pag
 def moveandclick(x_cord,y_cord):
     pag.moveTo(x_cord,y_cord)
     #pag.leftClick(x_cord,y_cord)
+# enter coordinates in the parameters
 moveandclick(1695,830)
